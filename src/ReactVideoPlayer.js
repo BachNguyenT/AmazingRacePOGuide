203edeea2bdacc079c73ff6859dde33fc0af9272
Bach Nguyen <bachesnguyen@gmail.com>
2024-03-02 14:48:37 +0700

    second commit

---
+++ src/ReactVideoPlayer.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { DefaultPlayer as Video } from "react-html5video";
+import "react-html5video/dist/styles.css";
+// import "./Video.css";
+
+import modelVideo from "./assets/POGuide.mp4";
+import thum from "./assets/POthum.jpg";
+
+const ReactVideoPlayer = () => {
+  return (
+    <div className="Div">
+      <Video
+        autoPlay
+        loop
+        poster={thum}
+        style={{ height: "100vh" }}
+        onCanPlayThrough={() => {
+          console.log("out");
+        }}
+      >
+        <source src={modelVideo} type="video/webm" />
+      </Video>
+    </div>
+  );
+};
+
+export default ReactVideoPlayer;
